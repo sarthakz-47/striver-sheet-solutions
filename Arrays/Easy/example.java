93c7adfd19a6d62ae175fbaa72d8905da6876b10
sarthakz-47 <sarthakz.4100@gmail.com>
2026-06-12 23:41:31 +0530

    commit changes

---
+++ Arrays/Easy/example.java
@@ -0,0 +1,11 @@
+public class example {
+      static int sum(int a, int b) {
+            return a + b;
+      };
+
+      public static void main(String[] args) {
+            int sum = sum(2, 3);
+            System.out.println(sum);
+      };
+
+}
